108e1aa853f7b58dc2f39d83b9af2c7b648cac0c
Acer Laptop <asn.nazarov.asn@gmail.com>
2020-12-06 22:26:16 +0300

    'init'

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import SignIn from "./pages/SignIn";
+
+function App() {
+  return (
+    <div className="App">
+     123
+       <SignIn />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {ThemeProvider} from '@material-ui/core';
+
+import './index.css';
+import theme from "./theme";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
